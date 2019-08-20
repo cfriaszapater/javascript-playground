89cd59f7d4a02fc02c152e63bda9314c650868bf
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-20 16:48:15 +0200

    refactor: distinguish test names

--- challenges/test/sockMerchant.spec.js
+++ challenges/test/sockMerchant.spec.js
@@ -7,7 +7,7 @@ const expect = require('expect');
 const sockMerchant = require('../sockMerchant');
 
 describe('calculate number of matching pairs of socks', function () {
-  it('should return 2 matching pairs of socks', function () {
+  it('should return 2 matching pairs of socks on short array', function () {
     var numberOfPairs = sockMerchant.sockMerchant(5, [1, 1, 2, 2, 3]);
     expect(numberOfPairs).toBe(2);
   });
@@ -17,7 +17,7 @@ describe('calculate number of matching pairs of socks', function () {
     expect(numberOfPairs).toBe(0);
   });
 
-  it('should return 2 matching pairs of socks', function () {
+  it('should return 2 matching pairs of socks on medium array', function () {
     var numberOfPairs = sockMerchant.sockMerchant(7, [1, 2, 3, 4, 3, 2, 7]);
     expect(numberOfPairs).toBe(2);
   });
