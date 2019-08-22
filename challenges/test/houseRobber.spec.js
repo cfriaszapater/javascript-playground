e67b5a65e4f7470121f18f9f4f1652795ebf389e
c.friaszapater <c.friaszapater@orwellg.com>
2019-08-22 08:01:40 +0200

    refactor: better test names

--- challenges/test/houseRobber.spec.js
+++ challenges/test/houseRobber.spec.js
@@ -7,18 +7,18 @@ require('chai').should();
 const houseRobber = require('../houseRobber');
 
 describe('Dynamic programming - Determine the maximum sum of non-adjacent elements', function () {
-  it('should return 0 on empty array', function () {
+  it('should be 0 given empty array', function () {
     houseRobber.houseRobber([]).should.eq(0);
   });
-  it('should return element on array with one element', function () {
+  it('should be element given one element', function () {
     houseRobber.houseRobber([0]).should.eq(0);
     houseRobber.houseRobber([13]).should.eq(13);
   });
-  it('should return max element on array with two elements', function () {
+  it('should be max element given two elements', function () {
     houseRobber.houseRobber([3, 4]).should.eq(4);
     houseRobber.houseRobber([1, 1]).should.eq(1);
   });
-  it('Should return 11', function () {
+  it('Should sum two non-adjacent given four elements', function () {
     houseRobber.houseRobber([1, 7, 9, 4]).should.eq(11);
   });
 });
